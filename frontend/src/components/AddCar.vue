6271662493bdef3748600f686b8cc05e69557ee4
Rodrigo Santos <rodrigosc.pi@gmail.com>
2021-02-18 17:25:46 -0300

    frontend: adds component for adding car

---
+++ frontend/src/components/AddCar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="add-car">
+    <form class="ui form">
+      <div class="fields">
+        <div class="four wide field">
+          <label>Marca</label>
+          <input type="text" name="marca" placeholder="Marca">
+        </div>
+
+        <div class="four wide field">
+          <label>Ano</label>
+          <input type="number" name="ano" placeholder="Ano">
+        </div>
+
+        <div class="two wide field">
+          <button class="ui primary button submit-button">Salvar</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddCar"
+};
+</script>
+
+<style scoped>
+
+</style>
