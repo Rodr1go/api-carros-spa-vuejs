89412b44e050613ab90bcc9f7cab3c957a123311
Rodrigo Santos <rodrigosc.pi@gmail.com>
2021-02-18 17:22:30 -0300

    frontend: adds component for listing cars

---
+++ frontend/src/components/ListCar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="list-car">
+    <div class="data">
+      <table class="ui celled table">
+        <thead>
+          <tr>
+            <th style="width: 50px; text-align: center;">#</th>
+            <th>Marca</th>
+            <th>Modelo</th>
+            <th>Ano</th>
+            <th style="width: 148px;">Ação</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <Car 
+            v-for="carro in carros"
+            :key="carro.id"
+            :carro="carro"
+            @onDelete="onDelete"
+          />
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import Car from "./Car";
+export default {
+  name: "ListCar",
+  components: {
+    Car
+  },
+  props: {
+    carros: {
+      type: Array
+    }
+  },
+  methods: {
+    onDelete(id) {
+      // window.console.log("Deleta carro" + id);
+      this.$emit("onDelete", id);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
