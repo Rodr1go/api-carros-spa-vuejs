e2ad01ba38e1c17e1f87aea4229d1a5e1478a10e
Rodrigo Santos <rodrigosc.pi@gmail.com>
2021-02-18 17:25:21 -0300

    frontend: adds car component

---
+++ frontend/src/components/Car.vue
@@ -0,0 +1,33 @@
+<template>
+  <tr>
+    <td>{{ carro.id }}</td>
+    <td>{{ carro.marca }}</td>
+    <td>{{ carro.modelo }}</td>
+    <td>{{ carro.ano }}</td>
+    <td>
+      <button class="mini ui blue button">Edit</button>
+      <button class="mini ui red button" @click="onDelete">Delete</button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "Car",
+  props: {
+    carro: {
+      type: Object
+    }
+  },
+  methods: {
+    onDelete() {
+      // window.console.log("customer list delete " + this.carro.id);
+      this.$emit("onDelete", this.carro.id);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
