e359e997ee88a9f17ef6f518b7abf02f9ef3bba1
Rodrigo Santos <rodrigosc.pi@gmail.com>
2021-02-22 19:18:47 -0300

    Add routes to the car controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\CarroController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+
+Route::prefix('v1')->group(function() {
+    Route::get('/ping', function() {
+        return response()->json(['message' => 'Servidor em funcionamento...']);
+    });
+
+    Route::apiResource('carros', CarroController::class);
 });
